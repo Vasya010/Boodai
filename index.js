1bac8f0517cb58c0ef877b5be1317a60bb804895
Vasya010 <sdsasdsadsa5@gmail.com>
2025-11-10 00:04:43 +0600

    545487878822211

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-//11
+
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key_very_secure_random_string';
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '7639223015:AAGdo2oB_uL4pEqXTnnepR4IpwsTSh2_UyY';
 const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY || 'GIMZKRMOGP4F0MOTLVCE';
 const S3_SECRET_KEY = process.env.S3_SECRET_KEY || 'WvhFfIzzCkITUrXfD8JfoDne7LmBhnNzDuDBj89I';
-const MYSQL_HOST = process.env.MYSQL_HOST || 'vh438.timeweb.ru';
-const MYSQL_USER = process.env.MYSQL_USER || 'ch79145_pizza';
+const MYSQL_HOST = process.env.MYSQL_HOST || 'vh446.timeweb.ru';
+const MYSQL_USER = process.env.MYSQL_USER || 'cz45780_pizzaame';
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || 'Vasya11091109';
-const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'ch79145_pizza';
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'cz45780_pizzaame';
 // Локальный SMS Gateway (на вашем сервере)
 const SMS_GATEWAY_URL = process.env.SMS_GATEWAY_URL || 'https://vasya010-boodai-b492.twc1.net/sms/send';
 const SMS_GATEWAY_API_KEY = process.env.SMS_GATEWAY_API_KEY || '';
@@ -715,7 +715,7 @@ function initializeServer(callback) {
           });
         }
         function createAdminUser() {
-          connection.query('SELECT * FROM users WHERE email = ?', ['admin@boodai.com'], (err, users) => {
+          connection.query('SELECT * FROM users WHERE email = ?', ['admin@ameranpizza.com'], (err, users) => {
             if (err) {
               connection.release();
               return callback(err);
@@ -728,7 +728,7 @@ function initializeServer(callback) {
                 }
                 connection.query(
                   'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-                  ['Admin', 'admin@boodai.com', hashedPassword],
+                  ['Admin', 'admin@ameranpizza.com', hashedPassword],
                   (err) => {
                     if (err) {
                       connection.release();
